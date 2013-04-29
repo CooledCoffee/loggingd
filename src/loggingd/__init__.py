062f397de1fb90a2f058b16b75a7a526e1de2af4
CooledCoffee <cooledcoffee@CoffeeStudio.(none)>
2013-04-29 12:43:20 +0800

    add handlers functions

--- src/loggingd/__init__.py
+++ src/loggingd/__init__.py
@@ -6,6 +6,19 @@ from loggingd.decorators import log_enter, log_return, log_error
 from loggingd.util import disable_module_log
 import logging
 
+def add_console_handler(level, fmt='%(message)s'):
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter(fmt))
+    getLogger().addHandler(handler)
+    
+def add_file_handler(level, path, fmt='[%(asctime)s] [%(levelname)s] [%(process)d:%(threadName)s] [%(name)s:%(funcName)s:%(lineno)d]\n%(message)s'):
+    logger = getLogger()
+    handler = logging.FileHandler(path)
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter(fmt))
+    logger.addHandler(handler)
+    
 def init(level=logging.INFO):
     logging.getLogger().setLevel(level)
     util.patch_logging()
