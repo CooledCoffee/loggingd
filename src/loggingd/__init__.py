1e46b8d750deef8024d6fcfdc19715598012740d
CooledCoffee <cooledcoffee@CoffeeStudio.(none)>
2013-04-29 12:40:19 +0800

    minor change

--- src/loggingd/__init__.py
+++ src/loggingd/__init__.py
@@ -1,12 +1,14 @@
 # -*- coding: UTF-8 -*-
 from decorated import Function
+from logging import getLogger, DEBUG, INFO, WARN, ERROR, CRITICAL
 from loggingd import util
 from loggingd.decorators import log_enter, log_return, log_error
+from loggingd.util import disable_module_log
 import logging
 
 def init(level=logging.INFO):
     logging.getLogger().setLevel(level)
     util.patch_logging()
-    util.disable_module_log(Function.__module__) #@UndefinedVariable
-    util.disable_module_log('loggingd.decorators')
+    disable_module_log(Function.__module__) #@UndefinedVariable
+    disable_module_log('loggingd.decorators')
     
