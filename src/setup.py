61b05c69521e403df10aad01b7661dcc096246b7
CooledCoffee <cooledcoffee@CoffeeStudio.(none)>
2013-04-30 08:45:37 +0800

    add decorated dependency in setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,10 +3,13 @@ from distutils.core import setup
 
 setup(
     name='loggingd',
-    version='1.0.1',
+    version='1.0.2',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     description='Logging using decorators.',
     packages=['loggingd'],
     url='https://github.com/CooledCoffee/loggingd/',
+    install_requires=[
+        'decorated >= 1.0.0',
+    ],
 )
