b4501ba999f58d4609ee66fac0afc8b1e8bcf092
CooledCoffee <limc@smvp.cn>
2013-10-29 14:29:15 +0800

    increase version

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='loggingd',
-    version='1.0.3',
+    version='1.0.4',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
