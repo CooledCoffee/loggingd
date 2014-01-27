15fddda331e9c4a76ac77de9894ff57e4f7dd992
TedWantsMore <TedWantsMore@gmx.com>
2014-01-26 16:09:20 -0800

    import log_exit in __ini__.py
    You are missing the import log_exit in __init__.py
    
    Also one more thing. On your examples I get 'No handlers could be found for logger "__main__"'. 
    So none of the decorators actually get called,so 
    I called logging.basicConfig() at the top of my script and it worked!

--- src/loggingd/__init__.py
+++ src/loggingd/__init__.py
@@ -3,7 +3,7 @@ from decorated.base import function
 from decorated.decorators import events
 from logging import getLogger, DEBUG, INFO, WARN, ERROR, CRITICAL
 from loggingd import util
-from loggingd.decorators import log_enter, log_return, log_error
+from loggingd.decorators import log_enter, log_return, log_error,log_exit
 from loggingd.util import disable_module_log
 import logging
 
