f8bbccf243221587547bdb397c59f421d397c504
CooledCoffee <CooledCoffee@gmail.com>
2017-08-01 17:57:44 +0800

    change url

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
     ],
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
-    url='https://package-insights.appspot.com/packages/loggingd'
+    url='https://github.com/cooledcoffee/loggingd'
 )
