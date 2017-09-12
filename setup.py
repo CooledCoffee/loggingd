b7f14d83eb1e34d2d7b6d7ba172641312b2d778d
CooledCoffee <CooledCoffee@gmail.com>
2017-09-12 17:12:25 +0800

    version 1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='loggingd',
-    version='1.2.9',
+    version='1.3',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
