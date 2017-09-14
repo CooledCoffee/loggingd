106ffa84b3934a11d98d05326881931072201695
CooledCoffee <CooledCoffee@gmail.com>
2017-09-14 16:35:45 +0800

    fix python3 compatibility

--- src/loggingd/util.py
+++ src/loggingd/util.py
@@ -11,16 +11,26 @@ class LoggingSession(six.with_metaclass(ContextMeta)):
 
 def patch():
     original_log = Logger._log
-    def _log(self, level, msg, args, exc_info=None, extra=None):
+    def _log(self, level, msg, args, exc_info=None, extra=None, stack_info=False):
         session = LoggingSession.current()
         if session is not None:
             extra = extra or {}
             extra.update(session.fields)
-        return original_log(self, level, msg, args, exc_info=exc_info, extra=extra)
+        if stack_info:
+            # for python3
+            return original_log(self, level, msg, args, exc_info=exc_info, extra=extra, stack_info=True)
+        else:
+            # for python2 & python3 without stack_info
+            return original_log(self, level, msg, args, exc_info=exc_info, extra=extra)
     Logger._log = _log
 
-    def _makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None):
-        rv = LogRecord(name, level, fn, lno, msg, args, exc_info, func)
+    def _makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None, sinfo=None):
+        if sinfo is not None:
+            # for python3
+            rv = LogRecord(name, level, fn, lno, msg, args, exc_info, func, sinfo)
+        else:
+            # for python2 & python3 without sinfo
+            rv = LogRecord(name, level, fn, lno, msg, args, exc_info, func)
         if extra is not None:
             rv.__dict__.update(extra)
         return rv
